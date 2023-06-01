0996323b38b96d390184e321db88811ee1bf4329
Kosenkov Ivan <bouncer77rus@gmail.com>
2023-06-01 21:08:41 +0300

    Добавил чтение, сортировку и запись

--- mdm_sort/src/main/java/ru/pczveer/mdm_sort/CommandLineRunnerImpl.java
+++ mdm_sort/src/main/java/ru/pczveer/mdm_sort/CommandLineRunnerImpl.java
@@ -3,7 +3,12 @@ package ru.pczveer.mdm_sort;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
@@ -12,5 +17,52 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
     public void run(String... args) {
         System.out.println("In CommandLineRunnerImpl ");
         Arrays.stream(args).forEach(System.out::println);
+        Map<String, String> map = readMdmUnicodeClassic(args[0]);
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+        writeMdmUnicodeClassic(map);
+
+    }
+
+    public static void writeMdmUnicodeClassic(Map<String, String> map) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                writer.write(entry.getKey() + "-" + entry.getValue() + "\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Map<String, String> readMdmUnicodeClassic(String fileName) {
+
+        Map<String, String> map = new TreeMap<>();
+
+        File file = new File(fileName);
+
+        try (FileInputStream fis = new FileInputStream(file);
+             InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+             BufferedReader reader = new BufferedReader(isr)
+        ) {
+
+            String str;
+            while ((str = reader.readLine()) != null) {
+                List<String> stringList = List.of(str.split("-", 2));
+                if (stringList.size() != 2) {
+                    System.out.println("Error:");
+                    stringList.forEach(System.out::println);
+                    System.out.println();
+                } else {
+                    map.put(stringList.get(0), stringList.get(1));
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return map;
+
     }
 }
--- mdm_sort/src/test/java/ru/pczveer/mdm_sort/MdmSortApplicationTests.java
+++ mdm_sort/src/test/java/ru/pczveer/mdm_sort/MdmSortApplicationTests.java
@@ -1,5 +1,6 @@
 package ru.pczveer.mdm_sort;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,9 @@ class MdmSortApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+
+		Assertions.assertEquals();
 	}
 
 }
