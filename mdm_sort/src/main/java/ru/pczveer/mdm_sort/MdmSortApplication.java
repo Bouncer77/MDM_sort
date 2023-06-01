83c21ef4a1ff66bde1edb54d44eabcbcba164d9c
Kosenkov Ivan <i.kosenkov77@yandex.ru>
2023-06-01 18:41:23 +0300

    2

--- mdm_sort/src/main/java/ru/pczveer/mdm_sort/MdmSortApplication.java
+++ mdm_sort/src/main/java/ru/pczveer/mdm_sort/MdmSortApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MdmSortApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MdmSortApplication.class, args); 
+        SpringApplication.run(MdmSortApplication.class, args);
     }
 }
